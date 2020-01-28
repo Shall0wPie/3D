54232686515e83e37aa8eca9596fb14279d75f11
VirgiliusDagon <36106295+VirgiliusDagon@users.noreply.github.com>
2020-01-28 19:23:06 +0500

    init commit

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] [Range(0f, 1f)] private float smoothMove;
+    [SerializeField] private float rotationSpeed;
+    [SerializeField] private Vector3 offset;
+    [SerializeField] private bool enableRotation;
+
+    private Vector3 deltaOffset;
+    private Vector3 currentVelocity;
+    private float yRot = 0;
+    private float xRot = 0;
+
+    private void Awake()
+    {
+        if (target == null)
+            target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void Update()
+    {
+        if (enableRotation || Input.GetMouseButton(1))
+        {
+            yRot += Input.GetAxis("Mouse X") * rotationSpeed;
+            xRot -= Input.GetAxis("Mouse Y") * rotationSpeed;
+            xRot = Mathf.Clamp(xRot, -25, 75);
+
+            Vector3 rotation = new Vector3(xRot, yRot, 0);
+
+            transform.eulerAngles = rotation;
+        }
+
+        deltaOffset = transform.rotation * offset;
+
+        Vector3 desiredPos = target.position + deltaOffset;
+
+        transform.position = Vector3.Slerp(transform.position, desiredPos, smoothMove);
+
+        //transform.position = Vector3.SmoothDamp(transform.position, desiredPos, ref currentVelocity, smoothMove);
+        //transform.position = desiredPos;
+    }
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerControl : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private Transform relativeTransform;
+    [SerializeField] private float rotationSpeed;
+    public bool enableRotation;
+
+    private Rigidbody rb;
+    private float vertical;
+    private float horizontal;
+    private Vector3 horizontalMove;
+    private Vector3 verticalMove;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        if (relativeTransform == null)
+            relativeTransform = transform;
+    }
+
+    void Update()
+    {
+        #region Movement
+        vertical = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
+
+        horizontalMove = (Vector3.ProjectOnPlane(relativeTransform.forward, Vector3.up).normalized * vertical
+                          + Vector3.ProjectOnPlane(relativeTransform.right, Vector3.up).normalized * horizontal) * (speed * Time.deltaTime);
+        verticalMove = Vector3.up * rb.velocity.y;
+
+        Debug.Log(horizontalMove);
+        rb.velocity = horizontalMove + verticalMove;
+        #endregion
+
+        #region Rotation
+        if (enableRotation)
+        {
+            float y = Input.GetAxis("Mouse X") * rotationSpeed;
+
+            Vector3 rotation = new Vector3(0, -y, 0);
+
+            transform.eulerAngles -= rotation;
+        }
+        #endregion
+
+        #region Loading
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            SceneManager.LoadScene("Level0");
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+            SceneManager.LoadScene("Level1");
+        #endregion
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        if (rb != null)
+        {
+            Gizmos.DrawRay(transform.position, rb.velocity);
+        }
+    }
+}
